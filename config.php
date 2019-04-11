54d0f4e12cfcb9638a5ff0d2ec24de04e3d673b3
Xstr00mZ <firdhoarianza@yahoo.com>
2019-04-12 06:41:41 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$uid="Facebook_1983301778445187";
+$raw_uid="ab935be2-6022-4865-ae94-bde8abdb2a2e";
+$user_name="Firdho+Arianza";
+$invite_code="F95B8gC7n9";
+$own_mac="cc%3A2d%3A83%3A82%3Ab3%3Acf";
+$android_id="1ce66e02a8603a4";
+$device_type="OPPO++A1603";
+
+
+?>
